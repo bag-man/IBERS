0cd433eb25be03bb53bdd334e989bc166bb0bd41
bag-man <garland.owen@gmail.com>
2014-08-08 16:47:05 +0100

    JSON kinda

--- script.py
+++ script.py
@@ -1,19 +1,24 @@
 #!/bin/bash/python
+import csv
 import os
 import gzip
 
 
 def getData(file): 
+  source = os.path.basename(file)
   file = open(file, 'r')
   dataList = []
 
   for line in file:
       filename = line.rstrip()
+      size = os.path.getsize(filename)
       gzFile = gzip.open(filename, 'r')
       headerLine = gzFile.readline().rstrip()
-      dataList.append([filename, headerLine, os.path.getsize(filename)])
+      dataList.append({'filePath': filename, 'header': headerLine,  'size': size, 'source': source})
+      
   
   return dataList
 
 quoatsFiles = getData("/ibers/ernie/home/owg1/data/quoats")
 repoFiles = getData("/ibers/ernie/home/owg1/data/repo")
+allData = quoatsFiles + repoFiles
