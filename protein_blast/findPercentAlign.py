445d31be727f5635c31c32a23f9aa6f5b559d01a
Owen Garland <owg1@aber.ac.uk>
2014-08-14 15:48:28 +0100

    Update findPercentAlign.py

--- protein_blast/findPercentAlign.py
+++ protein_blast/findPercentAlign.py
@@ -1,38 +1,23 @@
 from rjv.fasta import next_fasta
-import matplotlib.pyplot as plt
 from numpy.random import normal
-import csv
+import matplotlib.pyplot as plt
+import csv, sys
 
-# blastFile = "oatsBarleyBlast.tsv"
-# blastFile = "oatsBrachyBlast.tsv"
-# blastFile = "oatsSbiBlast.tsv"
-# blastFile = "oatsOstaviaBlast.tsv"
+blastFiles=\
+	  '''
+	    inputs/oatsBarleyBlast.tsv
+	    inputs/oatsBrachyBlast.tsv
+	    inputs/oatsSbiBlast.tsv
+	    inputs/oatsOstaviaBlast.tsv
+	  '''.split()
 
-# dataBaseFile = "Sbicolor_79_protein_primaryTranscriptOnly.fa"
-# dataBaseFile = "barley_HighConf_genes_MIPS_23Mar12_ProteinSeq.fa"
-# dataBaseFile = "Bdistachyon_192_protein_primaryTranscriptOnly.fa"
-# dataBaseFile = "Osativa_204_protein_primaryTranscriptOnly.fa"
-
-oatsBlast = open(blastFile,'r')
-oatsBlast = csv.reader(oatsBlast, delimiter='\t')
-
-scoreField = 4
-
-"""
-Field 	  	Description
-0 	  	Query label.
-1 	  	Target (database sequence or cluster centroid) label.
-2 	  	Percent identity.
-3 	  	Alignment length.
-4 	  	Number of mismatches.
-5 	  	Number of gap opens.
-6 	  	1-based position of start in query. For translated searches (nucleotide queries, protein targets), query start<end for +ve frame and start>end for -ve frame.
-7 	  	1-based position of end in query.
-8 	  	1-based position of start in target. For untranslated nucleotide searches, target start<end for plus strand, start>end for minus strand.
-9 	  	1-based position of end in target.
-10 	  	E-value calculated using Karlin-Altschul statistics.
-11 	  	Bit score calculated using Karlin-Altschul statistics.
-"""
+dataBaseFiles=\
+	    '''
+	      inputs/barley_HighConf_genes_MIPS_23Mar12_ProteinSeq.fa
+	      inputs/Bdistachyon_192_protein_primaryTranscriptOnly.fa
+	      inputs/Sbicolor_79_protein_primaryTranscriptOnly.fa
+	      inputs/Osativa_204_protein_primaryTranscriptOnly.fa
+	    '''.split()
 
 def createList(fileName):
   resultList = {}
@@ -41,11 +26,10 @@ def createList(fileName):
 
   return resultList
 
-inputData = createList(dataBaseFile)
-
 def testBlastFile(fileName):
   bestHits = {}
-  percAlignList = []
+  outputList = []
+  scoreField = 4
 
   for line in fileName:
     if line[0] not in bestHits:
@@ -56,12 +40,21 @@ def testBlastFile(fileName):
   for key, value in bestHits.items():
     oatsID = bestHits[key][0]
     dataBaseID = bestHits[key][1]
+    bestHits[key][11] = bestHits[key][11].strip()
     alignLength = float(bestHits[key][scoreField])
     brachyGeneLength = float(inputData[bestHits[key][1]]['len'])
     percentAlign = (alignLength / brachyGeneLength) * 100
-    percAlignList.append(percentAlign)
-    print oatsID + "," + dataBaseID + "," + str(percentAlign)
+    bestHits[key].append(str(percentAlign))
+    outputList.append(bestHits[key])
 
-  return percAlignList
+  return outputList
 
-data = testBlastFile(oatsBlast)
+output = []
+for blast in blastFiles:
+  inputData = createList(dataBaseFiles[blastFiles.index(blast)])
+  blastFile = open(blast,'r')
+  blastFile = csv.reader(blastFile, delimiter='\t')
+  output = testBlastFile(blastFile)
+  myfile = open("outputs/output.csv", 'a')
+  wr = csv.writer(myfile)
+  wr.writerows(output)
